75bfb6f06844252d6d5d26bd71bf42af007c6d06
wntitus <wesn.titus@gmail.com>
2018-08-10 22:17:12 -0400

    added do what it says and restructuring

--- liri.js
+++ liri.js
@@ -9,77 +9,70 @@ let client = new Twitter(keys.twitter);
 let spotify = new Spotify(keys.spotify);
 
 let command = process.argv[2];
+let secondQuery = process.argv[3];
 
-switch(command) {
-    case "my-tweets" : 
-    callTweets();
-    break;
-    case "spotify-this-song" : 
-    callSpotify();
-    break;
-    case "movie-this" : 
-    callOMDB();
-    break;
-    case "do-what-it-says" : 
-    callRandom();
-    break;
-    default : console.log("Invalid command! Beep boop. LIRI does not compute.");
-}
+function liriBot(arg, arg2) {
+    if (arg === 'my-tweets') {
+            client.get('statuses/user_timeline', {screen_name: 'twitterapi', count: '20'}, function(error, tweets, response) {
+                if (error) {
+                    throw error;
+                }
+                console.log(tweets);
+            })
+    }
 
-function callTweets() {
-        client.get('statuses/user_timeline', {screen_name: 'twitterapi', count: '20'}, function(error, tweets, response) {
-            if (error) {
-                throw error;
+    if (arg === 'spotify-this-song') {
+            if (typeof arg2 === 'undefined') {
+                spotify.search({type: 'track', query: 'the sign ace of base'}, function(error, data) {
+                    if (error) {
+                        throw error;
+                    }
+                    console.log(data.tracks.items[0]);
+                }) 
+            } else {
+
+                spotify.search({type: 'track', query: arg2}, function(error, data) {
+                    if (error) {
+                        throw error;
+                    }
+                    console.log(data.tracks.items[0]);
+                })
             }
-            console.log(tweets);
-        })
-}
+    }
 
-function callSpotify() {
-        if (typeof process.argv[3] === 'undefined') {
-            spotify.search({type: 'track', query: 'the sign ace of base'}, function(error, data) {
+    if (arg === 'movie-this') {
+            if (typeof arg2 === 'undefined') {
+                request("http://www.omdbapi.com/?apikey=c33bdeed&t=mr+nobody", function(error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        console.log(JSON.parse(body, null, 2));
+                    } else if (error) {
+                        throw error;
+                    }
+                })
+            } else {
+
+                request("http://www.omdbapi.com/?apikey=c33bdeed&t=" + arg2, function(error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        console.log(JSON.parse(body, null, 2));
+                    } else if (error) {
+                        throw error;
+                    }
+                })
+            }
+    }
+
+    if (arg === 'do-what-it-says') {
+            fs.readFile('./random.txt', 'utf8', function(error, data) {
                 if (error) {
                     throw error;
                 }
-                console.log(data.tracks.items[0]);
-            }) 
-        } else {
-            let songQuery = process.argv[3];
-            spotify.search({type: 'track', query: songQuery}, function(error, data) {
-                if (error) {
-                    throw error;
-                }
-                console.log(data.tracks.items[0]);
+                let dataArr = data.split(",");
+                console.log(dataArr);
+                liriBot(dataArr[0], dataArr[1]);
             })
-        }
+    }
 }
 
-function callOMDB() {
-        if (typeof process.argv[3] === 'undefined') {
-            request("http://www.omdbapi.com/?apikey=c33bdeed&t=mr+nobody", function(error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    console.log(JSON.parse(body, null, 2));
-                } else if (error) {
-                    throw error;
-                }
-            })
-        } else {
-            let movieQuery = process.argv[3];
-            request("http://www.omdbapi.com/?apikey=c33bdeed&t=" + movieQuery, function(error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    console.log(JSON.parse(body, null, 2));
-                } else if (error) {
-                    throw error;
-                }
-            })
-        }
-}
+liriBot(command, secondQuery);
 
-function callRandom() {
-    fs.readFile('./random.txt', function(error, data) {
-        if (error) {
-            throw error;
-        }
-        let randCommand = data;
-    })
-}
+
