94633c9f4246bd35c042ef646e9d4daba0292b6e
wntitus <wesn.titus@gmail.com>
2018-08-10 21:52:56 -0400

    adding functions and switch

--- liri.js
+++ liri.js
@@ -3,55 +3,70 @@ let keys = require('./keys');
 let Twitter = require('twitter');
 let Spotify = require('node-spotify-api');
 let request = require('request');
+let fs = require('fs');
 
 let client = new Twitter(keys.twitter);
 let spotify = new Spotify(keys.spotify);
 
 let command = process.argv[2];
 
-if (command === "my-tweets") {
-    client.get('statuses/user_timeline', {screen_name: 'twitterapi', count: '20'}, function(error, tweets, response) {
-        if (error) {
-            throw error;
-        }
-        console.log(tweets);
-    })
-} else if (command === "spotify-this-song") {
+switch(command) {
+    case "my-tweets" : callTweets();
+    case "spotify-this-song" : callSpotify();
+    case "movie-this" : callOMDB();
+    case "do-what-it-says" : callRandom();
+    default : console.log("Invalid command! Beep boop. LIRI does not compute.");
+}
 
-    if (typeof process.argv[3] === 'undefined') {
-        spotify.search({type: 'track', query: 'the sign ace of base'}, function(error, data) {
+let callTweets = function() {
+        client.get('statuses/user_timeline', {screen_name: 'twitterapi', count: '20'}, function(error, tweets, response) {
             if (error) {
                 throw error;
             }
-            console.log(data.tracks.items[0]);
-        }) 
-    } else {
-        let songQuery = process.argv[3];
-        spotify.search({type: 'track', query: songQuery}, function(error, data) {
-            if (error) {
-                throw error;
-            }
-            console.log(data.tracks.items[0]);
+            console.log(tweets);
         })
-    }
-} else if (command === "movie-this") {
-    if (typeof process.argv[3] === 'undefined') {
-        request("http://www.omdbapi.com/?apikey=c33bdeed&t=mr+nobody", function(error, response, body) {
-            if (!error && response.statusCode === 200) {
-                console.log(JSON.parse(body, null, 2));
-            } else if (error) {
-                throw error;
-            }
-        })
-    } else {
-        let movieQuery = process.argv[3];
-        request("http://www.omdbapi.com/?apikey=c33bdeed&t=" + movieQuery, function(error, response, body) {
-            if (!error && response.statusCode === 200) {
-                console.log(JSON.parse(body, null, 2));
-            } else if (error) {
-                throw error;
-            }
-        })
-    }
+}
+
+let callSpotify = function() {
+        if (typeof process.argv[3] === 'undefined') {
+            spotify.search({type: 'track', query: 'the sign ace of base'}, function(error, data) {
+                if (error) {
+                    throw error;
+                }
+                console.log(data.tracks.items[0]);
+            }) 
+        } else {
+            let songQuery = process.argv[3];
+            spotify.search({type: 'track', query: songQuery}, function(error, data) {
+                if (error) {
+                    throw error;
+                }
+                console.log(data.tracks.items[0]);
+            })
+        }
+}
+
+let callOMDB = function() {
+        if (typeof process.argv[3] === 'undefined') {
+            request("http://www.omdbapi.com/?apikey=c33bdeed&t=mr+nobody", function(error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log(JSON.parse(body, null, 2));
+                } else if (error) {
+                    throw error;
+                }
+            })
+        } else {
+            let movieQuery = process.argv[3];
+            request("http://www.omdbapi.com/?apikey=c33bdeed&t=" + movieQuery, function(error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log(JSON.parse(body, null, 2));
+                } else if (error) {
+                    throw error;
+                }
+            })
+        }
+}
+
+let callRandom = function() {
 
 }
